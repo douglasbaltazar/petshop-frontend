1e5af721451d023dc82107ff07ceeb07fe5dfc09
Douglas Baltazar <douglasbaltazar1@gmail.com>
2025-02-08 21:20:31 -0300

    feat: register e login pages

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, ReactiveFormsModule],
+  providers: [AuthService],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       cpf: [''],
       password: ['']
@@ -20,6 +23,15 @@ export class LoginPageComponent {
   onSubmit(): void {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
+      this.authService
+      .login(this.loginForm.value.cpf, this.loginForm.value.password)
+      .subscribe({
+        next: () => {
+          console.log('deu certo')
+        },
+        error: () =>
+          console.log('deu erro')
+      });
     }
   }
 }
--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,16 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, ReactiveFormsModule],
+  providers: [AuthService],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
   registerForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
     this.registerForm = this.fb.group({
       cpf: [''],
       password: [''],
@@ -21,6 +24,15 @@ export class RegisterPageComponent {
   onSubmit(): void {
     if (this.registerForm.valid) {
       console.log(this.registerForm.value);
+      this.authService
+      .signup(this.registerForm.value.nome, this.registerForm.value.cpf, this.registerForm.value.password)
+      .subscribe({
+        next: () => {
+          console.log('deu certo')
+        },
+        error: () =>
+          console.log('deu erro')
+      });
     }
   }
 
